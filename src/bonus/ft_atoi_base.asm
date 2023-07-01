1adf262b663f8af3f56cdb419f6ec19752ba62ed
skienzle <steffen.kienzle5@googlemail.com>
2023-07-01 22:34:33 +0200

    Add the rest of ft_atoi_base

--- src/bonus/ft_atoi_base.asm
+++ src/bonus/ft_atoi_base.asm
@@ -4,6 +4,7 @@ global ft_atoi_base
 %include "platform_specific.inc"
 
 extern ft_strlen
+extern GET_ERRNO
 
 ; Prototype:
 ;    int ft_atoi_base(const char* str, const char* base)
@@ -14,6 +15,7 @@ extern ft_strlen
 ;   rcx = copy of str
 ;   r8 = copy of base
 ;   r9 = base_len
+;   r10 = sign
 ; Stack layout:
 ;   rbp + 0 = copy of str
 ;   rbp + 8 = copy of base
@@ -55,32 +57,82 @@ ft_atoi_base:
 
         lea rax, [rcx + 1] ; size_t j = i + 1
         .LOOP2:
-            cmp dl, BYTE [rsi + rax] ; c == base[j]
+            cmp BYTE [rsi + rcx], BYTE [rsi + rax] ; base[i] == base[j]
             je .RETURN_ZERO
 
             inc rax ; ++j
-            cmp rax, r9 ; j < base_len
+            cmp rax, r9 ; j != base_len
             jne .LOOP2
 
         inc rcx ; ++i
-        cmp rcx, r9 ; i < base_len
+        cmp rcx, r9 ; i != base_len
         jne .LOOP1
 
+; --------------- skip spacing characters --------------
+
+    mov rcx, rbp ; const char* s = str
+    .LOOP3:
+        movzx edi, BYTE [rcx] ; c = *s
+        call ft_isspace
+        test al, al
+        jz .BREAK3
+        inc rcx ; ++s
+        jmp .LOOP3
+    .BREAK3:
+
+; --------------- calculate the sign --------------
+
+    mov r10, 1 ; int sign = 1
+    .LOOP4:
+        cmp BYTE [rcx], 45 ; *s == '-'
+        jne .SKIP4
+        neg r10 ; sign = -sign
+        inc rcx ; ++s
+        jmp .LOOP4
+    .SKIP4:
+        cmp BYTE [rcx], 43 ; *s == '+'
+        jne .BREAK4
+        inc rcx ; ++s
+        jmp .LOOP4
+    .BREAK4:
+
+; --------------- calculate the number --------------
+
+    xor rax, rax ; int result = 0
+    .LOOP5:
+        xor rdx, rdx ; int digit = 0
+        movzx edi, BYTE [rcx] ; char c = *s
+        ; find the digit in the base
+        ; if it's not found the conversion ends
+        .LOOP6:
+            cmp dil, BYTE [rsi + rdx] ; c == base[digit]
+            je .BREAK6
+            cmp BYTE [rsi + rdx], 0 ; base[digit] == '\0'
+            je .BREAK5
+            inc rdx ; ++digit
+            jmp .LOOP6
+        .BREAK6:
     
+        imul rax, r9 ; result * base_len
+        jo .OVERFLOW
+        add rax, rdx ; result + digit
+        jo .OVERFLOW
+    .BREAK5:
 
-; --------------- skip spaces and sign --------------
+    imul rax, r10 ; result * sign
+    jo .OVERFLOW
 
-
-
-
-
-
-
-
-
+    pop rsi ; restore base
+    pop rdi ; and str
     mov rsp, rbp ; function epilogue
     pop rbp
     ret
+
+.OVERFLOW:
+    pop rsi ; restore base
+    pop rdi ; and str
+    call GET_ERRNO ; set errno to ERANGE
+    mov DWORD [rax], ERANGE_ERROR_CODE
 
 .RETURN_ZERO:
     mov rsp, rbp ; function epilogue
@@ -94,9 +146,9 @@ ft_atoi_base:
 ;   c = rdi
 
 ft_isspace:
-    cmp rdi, 32 ; c == ' '
+    cmp edi, 32 ; c == ' '
     sete dl
-    lea eax, [rdi - 9] ; substact 9 from c because '\t' has ascii code 9
+    lea eax, [edi - 9] ; substact 9 from c because '\t' has ascii code 9
     cmp eax, 4 ; c >= '\t' && c <= '\r'
     setbe al
     or eax, edx ; combine the results into eax
