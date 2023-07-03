85812cd0d930c01af1ef08fc0b3aa807400c9c77
skienzle <steffen.kienzle5@googlemail.com>
2023-07-03 21:39:43 +0200

    Add ft_list_remove_if

---
+++ src/bonus/ft_list_remove_if.asm
@@ -0,0 +1,95 @@
+section .text
+global ft_list_remove_if
+
+%include "platform_specific.inc"
+
+EXTERN FREE
+
+; Prototype:
+;   void ft_list_remove_if(t_list** list,
+;                          void* data_ref,
+;                          int (*cmp_func)(),
+;                          void (*free_func)(void*)
+;                         )
+; Parameters:
+;   list = rdi
+;   data_ref = rsi
+;   cmp_func = rdx
+;   free_func = rcx
+; Register layout:
+;   rbx = t_list* iterator
+;   r12 = t_list* previous node
+;   r13 = t_list* next node
+;   r14 = cmp_func
+;   r15 = free_func
+; Stack layout:
+;   rbp + 0 = data_ref
+;   rbp + 8 = list
+
+ft_list_remove_if:
+    test rdi, rdi
+    jz .RETURN
+
+    push rbp ; function prologue
+    mov rbp, rsp
+
+    push rsi ; save arguments on stack
+    push rdi
+    push rbx ; save registers
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov r14, rdx
+    mov r15, rcx
+
+    mov rbx, QWORD [rdi] ; t_list* it = *list
+    test rbx, rbx ; it == NULL
+    jz .BREAK1
+    xor r12, r12 ; t_list* prev = NULL
+
+    .LOOP1:
+        mov r13, QWORD [rbx + 8] ; t_list* next = it->next
+
+        xor rax, rax ; clear return register
+        mov rdi, QWORD [rbx]
+        mov rsi, QWORD [rbp + 0]
+        call r14 ; cmp_func(it->data, data_ref)
+
+        test eax, eax ; return_value != 0
+        jnz .NO_MATCH
+            test r12, r12 ; test whether it's the first node in the list
+            jz .IF1
+                mov QWORD [r12 + 8], r13 ; prev->next = next
+                jmp .ENDIF1
+            .IF1:
+                mov rdx, QWORD [rbp + 8]
+                mov QWORD [rdx], r13 ; *list = node
+            .ENDIF1:
+
+            mov rdi, QWORD [rbx]
+            call r15 ; free_func(it->data)
+            mov rdi, rbx
+            call FREE ; free(it)
+            xor rdi, rdi ; it = NULL
+        .NO_MATCH:
+
+        mov r12, rbx ; prev = it
+        mov rbx, r13 ; it = next
+        test rbx, rbx
+        jnz .LOOP1
+    .BREAK1:
+
+    pop r15 ; restore registers
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rdi
+    pop rsi
+
+    mov rsp, rbp ; function epilogue
+    pop rbp
+.RETURN:
+    ret
